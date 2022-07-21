834203d56f643d8a68d5067fde1999b311ab238a
staticanime <static@staticanime.net>
2022-07-21 08:51:06 +0100

    Scripted auto-commit on change (2022-07-21 08:51:06) by gitwatch.sh

--- update-klipper-all.sh
+++ update-klipper-all.sh
@@ -6,7 +6,7 @@ make clean KCONFIG_CONFIG=config.btt
 make menuconfig KCONFIG_CONFIG=config.btt
 make KCONFIG_CONFIG=config.btt
 read -p "BTT Octopus MCU firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-make flash FLASH_DEVICE=1d50:614e KCONFIG_CONFIG=config.btt
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_380033000A51373330333137-if00 KCONFIG_CONFIG=config.btt
 read -p "BTT Octopus MCU firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 make clean KCONFIG_CONFIG=config.rpi
