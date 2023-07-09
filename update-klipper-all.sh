8a04e329b724eb9cdf7e0e14a6b4bb20bd5da1a1
staticanime <static@staticanime.net>
2023-07-09 13:20:16 +0100

    Scripted auto-commit on change (2023-07-09 13:20:16) by gitwatch.sh

--- update-klipper-all.sh
+++ update-klipper-all.sh
@@ -6,12 +6,16 @@ echo -e -n '\e[0;0m'
 sudo systemctl stop klipper
 sudo systemctl status klipper
 
+################################################################################
+
 git pull
+
+################################################################################
 
 # Flash main MCU - BTT Octopus
 make clean KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
 make menuconfig KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
-make KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
+make -j4 KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
 
 echo -e -n "\e[0;33mBTT Octopus MCU firmware built, please check above for any errors. "
 echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
@@ -26,10 +30,12 @@ echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
 echo -e -n '\e[0;0m'
 read
 
+################################################################################
+
 # Flash secondary MCU - Fystec PITB
 make clean KCONFIG_CONFIG=klipper-fystec-pitb.config
 make menuconfig KCONFIG_CONFIG=klipper-fystec-pitb.config
-make KCONFIG_CONFIG=klipper-fystec-pitb.config
+make -j 4 KCONFIG_CONFIG=klipper-fystec-pitb.config
 
 echo -e -n "\e[0;33mFYSTEC PITB MCU firmware built, please check above for any errors. "
 echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
@@ -43,10 +49,12 @@ echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
 echo -e -n '\e[0;0m'
 read
 
+################################################################################
+
 # Flash tertiary MCU - Mellow SB2040
 make clean KCONFIG_CONFIG=klipper-mellow-fly-SB2040.config
 make menuconfig KCONFIG_CONFIG=klipper-mellow-fly-SB2040.config
-make KCONFIG_CONFIG=klipper-mellow-fly-SB2040.config
+make -j 4 KCONFIG_CONFIG=klipper-mellow-fly-SB2040.config
 
 echo -e -n "\e[0;33mMellow FLY SB2040 MCU firmware built, please check above for any errors. "
 echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
@@ -60,10 +68,12 @@ echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
 echo -e -n '\e[0;0m'
 read
 
+################################################################################
+
 # Flash Host MCU - Raspberry Pi
 make clean KCONFIG_CONFIG=klipper-raspberry-pi.config
 make menuconfig KCONFIG_CONFIG=klipper-raspberry-pi.config
-make KCONFIG_CONFIG=klipper-raspberry-pi.config
+make -j 4 KCONFIG_CONFIG=klipper-raspberry-pi.config
 
 echo -e -n "\e[0;33mRaspberry Pi MCU firmware flashed, please check above for any errors. "
 echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
@@ -77,9 +87,11 @@ echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
 echo -e -n '\e[0;0m'
 read
 
+################################################################################
+
 echo -e -n "\e[0;32mStarting Klipper Service."
 echo -e -n '\e[0;0m'
 sudo systemctl start klipper
 sudo systemctl status klipper
 
-exit
+exit 0
